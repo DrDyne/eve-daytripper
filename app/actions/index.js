1a9beba4046c00aee97d8a2325660d807bd48789
Remy Chaumard <rchaumard@touchtunes.com>
2017-09-03 14:38:50 -0400

    added PASTED action creator

--- app/actions/index.js
+++ app/actions/index.js
@@ -3,6 +3,9 @@ export const SET_BREADCRUMBS_COLOR = 'breadcrumbs:color:set'
 export const CHANGE_TAB = 'tab:change'
 export const SETTINGS_MENU_TOGGLE = 'menu:settings:toggle'
 export const SWAP_STOCK_AND_LOOT_LISTS = 'layout:lists:swap'
+export const PASTED = 'event:paste'
+export const ADD_STOCK = 'stock:add'
+export const REMOVE_STOCK = 'stock:remove'
 
 export const setBreadcrumbs = items => ({
   type: SET_BREADCRUMBS,
@@ -25,4 +28,9 @@ export const toggleSettingsMenu = () => ({
 
 export const swapListLayout = () => ({
   type: SWAP_STOCK_AND_LOOT_LISTS
+})
+
+export const pasted = clipboard => ({
+  type: PASTED,
+  clipboard
 })
