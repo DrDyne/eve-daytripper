176ae7c27ed2f9eb27566ddaf14d799911f3232a
r.chaumard@gmail.com <rchaumard@touchtunes.com>
2017-10-01 14:48:50 -0400

    added short route renderer -- added security class colors

--- app/actions/gps.js
+++ app/actions/gps.js
@@ -4,21 +4,32 @@ import {
 
 export const isWormhole = name => /^J[0-9]{6}$/.test(name)
 
+const byOriginId = id => route => route.origin.id === id
+
 export const createRouteFromPaste = () => (dispatch, getState, {api}) => {
   const { raw } = getState().history.lastPasted
-  const { favorites } = getState().gps
+  const { favorites, routes } = getState().gps
 
   if ( raw.length > 25 ) return
 
   return api.gps.identify(raw)
+  .then(origin => {
+    if ( routes.find(byOriginId(origin.id)) ) return Promise.reject('route already exists')
+    return origin
+  })
   .then(origin => Promise.all(favorites.map(destination => {
     return api.gps.route(origin, destination)
-    .then(route => (route[0].id === origin.id) ? route : route.reverse())
+    .then(route => (route[0].id === origin.id) ? route : route.reverse()) // sometimes ccp server reverses order /shrug
   })))
   .then(routes => {
     console.log('routes !', routes)
     routes.forEach(route => {
       dispatch(createRoute(route))
     })
+  })
+  .catch(err => {
+    if ( 'route already exists' !== err ) throw err
+    console.warn(err)
+    return Promise.resolve()
   })
 }
--- app/components/Gps/component.jsx
+++ app/components/Gps/component.jsx
@@ -140,7 +140,7 @@ const RouteCard = props => {
         {route.origin.name} {route.systems.length} {route.destination.name}
       </Typography>
       <Typography type="caption"> { route.origin.name} </Typography>
-      route goes here
+      <RoutePath systems={route.systems} />
       <Typography type="caption"> { route.destination.name} </Typography>
     </CardContent>
     <CardActions>
@@ -173,4 +173,68 @@ const GpsRoutesToFavorites = props => {
       .map(route => <RouteCard key={route.destination.id} route={route} />)
     }
   </div>)
+}
+
+const RoutePath = ({systems}) => {
+  const securityClass = sec => sec > 0.5 ? 'HS' : sec > 0 ? 'LS' : 'NS'
+  const short = systems.reduce((memo, system) => {
+    const secClass = securityClass(system.sec)
+
+    if ( !memo.systems.length ) return {
+      lastSec: secClass,
+      jumps: 1,
+      systems: [{
+        class: secClass,
+        jumps: 1,
+        worstSec: system.sec,
+        systems: [system],
+      }]
+    }
+
+    else if ( secClass !== memo.lastSec ) {
+      return Object.assign(memo, {
+        lastSec: secClass,
+        jumps: memo.jumps + 1,
+      }, {
+        systems: [ ...memo.systems, {
+          class: secClass,
+          jumps: 1,
+          worstSec: system.sec,
+          systems: [system],
+        } ]
+      })
+    }
+
+    else {
+      const lastClass = memo.systems.slice().pop()
+      return Object.assign(memo, {
+        jumps: memo.jumps + 1,
+        systems: [ ...memo.systems.slice(0, -1), {
+          class: lastClass.class,
+          jumps: lastClass.jumps + 1,
+          worstSec: Math.min(system.sec, lastClass.worstSec),
+          systems: [...lastClass.systems, system],
+        }]
+      })
+    }
+  }, {lastSec: null, systems: [], jumps: 0})
+
+  console.log(short)
+
+  return <div>
+    { short.systems.map(s => {
+      console.log(s.worstSec)
+      return <div
+        key={`${s.class}:${s.jumps}:${s.systems[0].name}-${s.systems.slice().pop().name}`}
+      >
+        <div
+          className={`sec-${s.worstSec.toString().slice(0,3).replace('.', '-')}`}
+          style={{
+            width: 10,
+            height: 10
+        }} />
+        { s.class }: {s.jumps}
+      </div>
+    }) }
+  </div>
 }
