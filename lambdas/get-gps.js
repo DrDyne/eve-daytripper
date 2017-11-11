5c2e8bc463cce422ebdff88924f889a636c85511
r.chaumard@gmail.com <rchaumard@mtl-osx-pla-rch.local>
2017-11-11 14:25:09 -0500

    fixed typo

--- lambdas/get-gps.js
+++ lambdas/get-gps.js
@@ -3,7 +3,7 @@ import { getGps, response } from './utils'
 exports.handler = (event, context, done) => {
   const username = event.requestContext.authorizer.claims['cognito:username']
 
-  getFleet(username)
+  getGps(username)
   .then(gps => {
     done(null, response.json(gps))
   })
--- lambdas/get-inventory.js
+++ lambdas/get-inventory.js
@@ -3,7 +3,8 @@ import { getInventory, response } from './utils'
 exports.handler = (event, context, done) => {
   const username = event.requestContext.authorizer.claims['cognito:username']
 
-  getInventory(username).then(inventory => {
+  getInventory(username)
+  .then(inventory => {
     done(null, response.json(inventory))
   })
 }
