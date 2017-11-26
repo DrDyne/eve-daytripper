a9db79bd19a299bbc889f99a85e4041cffe96349
r.chaumard@gmail.com <rchaumard@mtl-osx-pla-rch.local>
2017-11-26 12:07:44 -0500

    added s3 mocks

---
+++ app/store/s3/eve-daytripper-gps/bobby1.js
@@ -0,0 +1,722 @@
+export default
+var obj = {
+  favorites: [
+    {
+      id: 30000142,
+      name: 'Jita',
+      sec: 0.945913117
+    }
+  ],
+  avoidance: [],
+  routes: [
+    {
+      origin: {
+        id: 30002415,
+        name: 'Weld',
+        sec: 0.302036212
+      },
+      destination: {
+        id: 30000142,
+        name: 'Jita',
+        sec: 0.945913117
+      },
+      safest: {
+        jumps: 34,
+        systems: [
+          {
+            id: 30002415,
+            name: 'Weld',
+            sec: 0.302036212
+          },
+          {
+            id: 30002414,
+            name: 'Klingt',
+            sec: 0.302146752
+          },
+          {
+            id: 30002413,
+            name: 'Unertek',
+            sec: 0.304135127
+          },
+          {
+            id: 30002409,
+            name: 'Osvetur',
+            sec: 0.2475799
+          },
+          {
+            id: 30002408,
+            name: 'Fegomenko',
+            sec: 0.504198982
+          },
+          {
+            id: 30002399,
+            name: 'Orien',
+            sec: 0.80472537
+          },
+          {
+            id: 30002397,
+            name: 'Horaka',
+            sec: 0.619041752
+          },
+          {
+            id: 30002396,
+            name: 'Illamur',
+            sec: 0.282340277
+          },
+          {
+            id: 30002389,
+            name: 'Atlar',
+            sec: 0.37336748
+          },
+          {
+            id: 30002386,
+            name: 'Gelfiven',
+            sec: 0.559771147
+          },
+          {
+            id: 30002385,
+            name: 'Teonusude',
+            sec: 0.586313723
+          },
+          {
+            id: 30002570,
+            name: 'Magiko',
+            sec: 0.938302025
+          },
+          {
+            id: 30002573,
+            name: 'Vullat',
+            sec: 0.971362611
+          },
+          {
+            id: 30002543,
+            name: 'Eystur',
+            sec: 0.949231822
+          },
+          {
+            id: 30002053,
+            name: 'Hek',
+            sec: 0.549668267
+          },
+          {
+            id: 30002049,
+            name: 'Uttindar',
+            sec: 0.504430247
+          },
+          {
+            id: 30002048,
+            name: 'Bei',
+            sec: 0.564869229
+          },
+          {
+            id: 30002682,
+            name: 'Colelie',
+            sec: 0.512388828
+          },
+          {
+            id: 30002681,
+            name: 'Deltole',
+            sec: 0.540472801
+          },
+          {
+            id: 30002641,
+            name: 'Aufay',
+            sec: 0.523120378
+          },
+          {
+            id: 30002634,
+            name: 'Balle',
+            sec: 0.460889099
+          },
+          {
+            id: 30002633,
+            name: 'Du Annes',
+            sec: 0.647813951
+          },
+          {
+            id: 30004970,
+            name: 'Renyn',
+            sec: 0.898199338
+          },
+          {
+            id: 30004972,
+            name: 'Algogille',
+            sec: 0.925366203
+          },
+          {
+            id: 30002761,
+            name: 'Kassigainen',
+            sec: 0.915852314
+          },
+          {
+            id: 30002764,
+            name: 'Hatakani',
+            sec: 0.941060866
+          },
+          {
+            id: 30002765,
+            name: 'Sivala',
+            sec: 0.550531907
+          },
+          {
+            id: 30002768,
+            name: 'Uedama',
+            sec: 0.505440322
+          },
+          {
+            id: 30002800,
+            name: 'Haatomo',
+            sec: 0.609265501
+          },
+          {
+            id: 30002801,
+            name: 'Suroken',
+            sec: 0.729161139
+          },
+          {
+            id: 30002802,
+            name: 'Kusomonmon',
+            sec: 0.847299954
+          },
+          {
+            id: 30000139,
+            name: 'Urlen',
+            sec: 0.959995211
+          },
+          {
+            id: 30000144,
+            name: 'Perimeter',
+            sec: 0.953123231
+          },
+          {
+            id: 30000142,
+            name: 'Jita',
+            sec: 0.945913117
+          }
+        ],
+        bestSec: 0.971362611,
+        worstSec: 0.2475799
+      },
+      shortest: {
+        jumps: 21,
+        systems: [
+          {
+            id: 30002415,
+            name: 'Weld',
+            sec: 0.302036212
+          },
+          {
+            id: 30002419,
+            name: 'Aeditide',
+            sec: 0.214490969
+          },
+          {
+            id: 30002420,
+            name: 'Egbinger',
+            sec: 0.034555197
+          },
+          {
+            id: 30002394,
+            name: 'Muttokon',
+            sec: 0.207189994
+          },
+          {
+            id: 30002393,
+            name: 'Aedald',
+            sec: 0.250457749
+          },
+          {
+            id: 30002390,
+            name: 'Heild',
+            sec: 0.330762362
+          },
+          {
+            id: 30002387,
+            name: 'Bosena',
+            sec: 0.400467094
+          },
+          {
+            id: 30002385,
+            name: 'Teonusude',
+            sec: 0.586313723
+          },
+          {
+            id: 30002570,
+            name: 'Magiko',
+            sec: 0.938302025
+          },
+          {
+            id: 30002573,
+            name: 'Vullat',
+            sec: 0.971362611
+          },
+          {
+            id: 30002543,
+            name: 'Eystur',
+            sec: 0.949231822
+          },
+          {
+            id: 30002053,
+            name: 'Hek',
+            sec: 0.549668267
+          },
+          {
+            id: 30002723,
+            name: 'Otou',
+            sec: 0.333602901
+          },
+          {
+            id: 30002719,
+            name: 'Miroitem',
+            sec: 0.30519244
+          },
+          {
+            id: 30002718,
+            name: 'Rancer',
+            sec: 0.36466069
+          },
+          {
+            id: 30002691,
+            name: 'Crielere',
+            sec: 0.398449631
+          },
+          {
+            id: 30002644,
+            name: 'Ambeke',
+            sec: 0.500554907
+          },
+          {
+            id: 30002643,
+            name: 'Faurent',
+            sec: 0.536504194
+          },
+          {
+            id: 30002642,
+            name: 'Iyen-Oursta',
+            sec: 0.7795754
+          },
+          {
+            id: 30000144,
+            name: 'Perimeter',
+            sec: 0.953123231
+          },
+          {
+            id: 30000142,
+            name: 'Jita',
+            sec: 0.945913117
+          }
+        ],
+        bestSec: 0.971362611,
+        worstSec: 0.034555197
+      }
+    },
+    {
+      origin: {
+        id: 30002415,
+        name: 'Weld',
+        sec: 0.302036212
+      },
+      destination: {
+        id: 30005281,
+        name: 'Exit',
+        sec: 0.172100838
+      },
+      safest: {
+        jumps: 39,
+        systems: [
+          {
+            id: 30002415,
+            name: 'Weld',
+            sec: 0.302036212
+          },
+          {
+            id: 30002414,
+            name: 'Klingt',
+            sec: 0.302146752
+          },
+          {
+            id: 30002413,
+            name: 'Unertek',
+            sec: 0.304135127
+          },
+          {
+            id: 30002409,
+            name: 'Osvetur',
+            sec: 0.2475799
+          },
+          {
+            id: 30002408,
+            name: 'Fegomenko',
+            sec: 0.504198982
+          },
+          {
+            id: 30002399,
+            name: 'Orien',
+            sec: 0.80472537
+          },
+          {
+            id: 30002397,
+            name: 'Horaka',
+            sec: 0.619041752
+          },
+          {
+            id: 30002396,
+            name: 'Illamur',
+            sec: 0.282340277
+          },
+          {
+            id: 30002389,
+            name: 'Atlar',
+            sec: 0.37336748
+          },
+          {
+            id: 30002386,
+            name: 'Gelfiven',
+            sec: 0.559771147
+          },
+          {
+            id: 30002385,
+            name: 'Teonusude',
+            sec: 0.586313723
+          },
+          {
+            id: 30002570,
+            name: 'Magiko',
+            sec: 0.938302025
+          },
+          {
+            id: 30002573,
+            name: 'Vullat',
+            sec: 0.971362611
+          },
+          {
+            id: 30002543,
+            name: 'Eystur',
+            sec: 0.949231822
+          },
+          {
+            id: 30002053,
+            name: 'Hek',
+            sec: 0.549668267
+          },
+          {
+            id: 30002049,
+            name: 'Uttindar',
+            sec: 0.504430247
+          },
+          {
+            id: 30002048,
+            name: 'Bei',
+            sec: 0.564869229
+          },
+          {
+            id: 30002682,
+            name: 'Colelie',
+            sec: 0.512388828
+          },
+          {
+            id: 30002681,
+            name: 'Deltole',
+            sec: 0.540472801
+          },
+          {
+            id: 30002641,
+            name: 'Aufay',
+            sec: 0.523120378
+          },
+          {
+            id: 30002634,
+            name: 'Balle',
+            sec: 0.460889099
+          },
+          {
+            id: 30002633,
+            name: 'Du Annes',
+            sec: 0.647813951
+          },
+          {
+            id: 30004970,
+            name: 'Renyn',
+            sec: 0.898199338
+          },
+          {
+            id: 30005198,
+            name: 'Pakhshi',
+            sec: 0.845482997
+          },
+          {
+            id: 30005199,
+            name: 'Tar',
+            sec: 0.756677245
+          },
+          {
+            id: 30005200,
+            name: 'Tekaima',
+            sec: 0.594752422
+          },
+          {
+            id: 30005205,
+            name: 'Tarta',
+            sec: 0.498363576
+          },
+          {
+            id: 30005221,
+            name: 'Zoohen',
+            sec: 0.467727116
+          },
+          {
+            id: 30005289,
+            name: 'Nasreri',
+            sec: 0.525594427
+          },
+          {
+            id: 30005290,
+            name: 'Heorah',
+            sec: 0.537723493
+          },
+          {
+            id: 30005293,
+            name: 'Doza',
+            sec: 0.573759168
+          },
+          {
+            id: 30005243,
+            name: 'Madomi',
+            sec: 0.587495249
+          },
+          {
+            id: 30005244,
+            name: 'Gergish',
+            sec: 0.582955965
+          },
+          {
+            id: 30005246,
+            name: 'Imya',
+            sec: 0.518441185
+          },
+          {
+            id: 30005261,
+            name: 'Antem',
+            sec: 0.31288034
+          },
+          {
+            id: 30005262,
+            name: 'Djimame',
+            sec: 0.275478748
+          },
+          {
+            id: 30005264,
+            name: 'Angur',
+            sec: 0.177585126
+          },
+          {
+            id: 30005266,
+            name: 'Access',
+            sec: 0.164282976
+          },
+          {
+            id: 30005281,
+            name: 'Exit',
+            sec: 0.172100838
+          }
+        ],
+        bestSec: 0.971362611,
+        worstSec: 0.164282976
+      },
+      shortest: {
+        jumps: 36,
+        systems: [
+          {
+            id: 30002415,
+            name: 'Weld',
+            sec: 0.302036212
+          },
+          {
+            id: 30002419,
+            name: 'Aeditide',
+            sec: 0.214490969
+          },
+          {
+            id: 30002420,
+            name: 'Egbinger',
+            sec: 0.034555197
+          },
+          {
+            id: 30002394,
+            name: 'Muttokon',
+            sec: 0.207189994
+          },
+          {
+            id: 30002393,
+            name: 'Aedald',
+            sec: 0.250457749
+          },
+          {
+            id: 30002390,
+            name: 'Heild',
+            sec: 0.330762362
+          },
+          {
+            id: 30002387,
+            name: 'Bosena',
+            sec: 0.400467094
+          },
+          {
+            id: 30002385,
+            name: 'Teonusude',
+            sec: 0.586313723
+          },
+          {
+            id: 30002570,
+            name: 'Magiko',
+            sec: 0.938302025
+          },
+          {
+            id: 30002568,
+            name: 'Onga',
+            sec: 0.953513758
+          },
+          {
+            id: 30002529,
+            name: 'Gyng',
+            sec: 0.79814297
+          },
+          {
+            id: 30002530,
+            name: 'Avesber',
+            sec: 0.753183657
+          },
+          {
+            id: 30002507,
+            name: 'Abudban',
+            sec: 0.732447725
+          },
+          {
+            id: 30002506,
+            name: 'Osoggur',
+            sec: 0.53156829
+          },
+          {
+            id: 30002537,
+            name: 'Amamake',
+            sec: 0.438761248
+          },
+          {
+            id: 30002542,
+            name: 'Auga',
+            sec: 0.385483821
+          },
+          {
+            id: 30003068,
+            name: 'Kourmonen',
+            sec: 0.36348016
+          },
+          {
+            id: 30003067,
+            name: 'Huola',
+            sec: 0.370248588
+          },
+          {
+            id: 30003065,
+            name: 'Otelen',
+            sec: 0.784819394
+          },
+          {
+            id: 30003520,
+            name: 'Mahrokht',
+            sec: 0.859905816
+          },
+          {
+            id: 30003521,
+            name: 'Alkabsi',
+            sec: 0.662152806
+          },
+          {
+            id: 30003522,
+            name: 'Sarum Prime',
+            sec: 1
+          },
+          {
+            id: 30002187,
+            name: 'Amarr',
+            sec: 1
+          },
+          {
+            id: 30005038,
+            name: 'Kor-Azor Prime',
+            sec: 0.913908135
+          },
+          {
+            id: 30005036,
+            name: 'Amdonen',
+            sec: 0.640654726
+          },
+          {
+            id: 30005035,
+            name: 'Ami',
+            sec: 0.438170999
+          },
+          {
+            id: 30005030,
+            name: 'Fensi',
+            sec: 0.396833384
+          },
+          {
+            id: 30005031,
+            name: 'Nebian',
+            sec: 0.351496646
+          },
+          {
+            id: 30005236,
+            name: 'Noranim',
+            sec: 0.337220964
+          },
+          {
+            id: 30005237,
+            name: 'Chej',
+            sec: 0.28344913
+          },
+          {
+            id: 30005239,
+            name: 'Aring',
+            sec: 0.267323714
+          },
+          {
+            id: 30005261,
+            name: 'Antem',
+            sec: 0.31288034
+          },
+          {
+            id: 30005262,
+            name: 'Djimame',
+            sec: 0.275478748
+          },
+          {
+            id: 30005264,
+            name: 'Angur',
+            sec: 0.177585126
+          },
+          {
+            id: 30005266,
+            name: 'Access',
+            sec: 0.164282976
+          },
+          {
+            id: 30005281,
+            name: 'Exit',
+            sec: 0.172100838
+          }
+        ],
+        bestSec: 1,
+        worstSec: 0.034555197
+      }
+    }
+  ],
+  origins: [
+    {
+      id: 30002415,
+      name: 'Weld',
+      sec: 0.302036212
+    }
+  ],
+}
